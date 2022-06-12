556d52e88c72edf37f1d04ea47ce13ecef22dd04
Qold <qoldprogram@gmail.com>
2022-06-12 16:39:59 +0300

    implemented menu

--- app/src/main/java/com/example/lifeline/presentation/BaseActivity.kt
+++ app/src/main/java/com/example/lifeline/presentation/BaseActivity.kt
@@ -2,18 +2,23 @@ package com.example.lifeline.presentation
 
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.lifeline.R
 import timber.log.Timber
 
-abstract class BaseActivity: AppCompatActivity() {
-    fun makeToast(text: String){
+abstract class BaseActivity : AppCompatActivity() {
+    fun makeToast(text: String) {
         Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
     }
 
-    fun log(text: String){
+    fun log(text: String) {
         Timber.i(text)
     }
 
-    fun logError(text: String){
+    fun logError(text: String) {
         Timber.e(text)
+    }
+
+    fun showDevelopmentMessage() {
+        Toast.makeText(this, getString(R.string.development_message), Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/lifeline/presentation/ui/DefaultActivity.kt
+++ app/src/main/java/com/example/lifeline/presentation/ui/DefaultActivity.kt
@@ -2,7 +2,6 @@ package com.example.lifeline.presentation.ui
 
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.lifeline.R
 import com.example.lifeline.databinding.DefaultActivityBinding
@@ -14,5 +13,33 @@ class DefaultActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.default_activity)
         binding.lifecycleOwner = this
+
+        binding.navView.setNavigationItemSelectedListener {
+            binding.drawer.closeDrawers()
+            when (it.itemId) {
+                R.id.itemMain -> {
+                    binding.fragmentContainerView.getFragment<NavHostFragment>().navController.navigate(
+                        R.id.diary_fragment
+                    )
+                    true
+                }
+                R.id.itemFill -> {
+                    binding.fragmentContainerView.getFragment<NavHostFragment>().navController.navigate(
+                        R.id.fill_fragment
+                    )
+                    true
+                }
+                R.id.itemStatistics ->{
+                    binding.fragmentContainerView.getFragment<NavHostFragment>().navController.navigate(
+                        R.id.statistics_fragment
+                    )
+                    true
+                }
+                else -> {
+                    showDevelopmentMessage()
+                    false
+                }
+            }
+        }
     }
 }
